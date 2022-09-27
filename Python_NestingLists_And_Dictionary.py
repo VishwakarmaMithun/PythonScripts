fce9d4ed95d5d00e1ee6a39d02f5ac2596f3fdcc
VishwakarmaMithun <114065167+VishwakarmaMithun@users.noreply.github.com>
2022-09-27 16:57:07 +0530

    Add files via upload

---
+++ Python_NestingLists_And_Dictionary.py
@@ -0,0 +1,33 @@
+#Nesting
+capitals = {
+    "France":"Paris",
+    "Germany":"Berlin"
+}
+
+#Nesting List Inside Dictionary example
+
+travel_log ={
+"France": {"Cities Visited":["Paris","Lille","Dijon"],
+            "Total_No_Visits":12},
+'Germany':["Berlin","Hambury","Stuttgart"],
+
+}
+
+#Nesting Dictionary with List
+
+travel_log_list =[
+{
+    "Country":"France",
+    "Cities_Visited": ["Paris","Lille","Dijon"],
+    "total_vists":12
+},
+{
+    "Country":"India",
+    "Cities_Visited": ["Delhi","Gurgaon","Mumbai"],
+    "total_vists":5
+},
+
+]
+
+print(travel_log_list)
+
