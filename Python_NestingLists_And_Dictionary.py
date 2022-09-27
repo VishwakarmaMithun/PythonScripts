14906fd4d2f10014f1ababb1f4f4cd4a829abbc4
VishwakarmaMithun <114065167+VishwakarmaMithun@users.noreply.github.com>
2022-09-27 18:06:16 +0530

    Add files via upload

--- Python_NestingLists_And_Dictionary.py
+++ Python_NestingLists_And_Dictionary.py
@@ -31,3 +31,14 @@ travel_log_list =[
 
 print(travel_log_list)
 
+#write a function will allow new record to be added to next dictionary
+def add_new_country(country_visited,times_visited,cities_visited):
+    new_country= {}
+    new_country["Country"]=country_visited;
+    new_country["total_vists"]=times_visited;
+    new_country["Cities_Visited"]=cities_visited;
+    travel_log_list.append(new_country)
+
+
+add_new_country("Russia",2,["Moscow","Saint Petersburg"]);
+print(travel_log_list)
