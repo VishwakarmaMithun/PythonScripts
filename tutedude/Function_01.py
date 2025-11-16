61ab454d9b805aa855c79ef712e58e99a7594fc4
VishwakarmaMithun <mitunv@live.com>
2025-11-16 12:40:39 +0530

    .

--- tutedude/Function_01.py
+++ tutedude/Function_01.py
@@ -21,9 +21,12 @@ greet_user('Mithun')
 def add(a,b):
     return a+b;
 
-
 def greet_default(name="Mithun"):
     print(f" Entered name is {name}")
+
+
+def student(name, age):
+    print("name is {} and age is {}".format(name,age))
 
 print("sum is ", add(5,1));
 
@@ -32,4 +35,22 @@ b1 = int(input("Enter value 2"))
 sum =  add(a1,b1)
 print("sum of value 1 and value 2 ", str(sum))
 
+#default argument
 print(greet_default());
+#keyword argument
+student(name='Mithun',age=15)
+def student_info(name,age,city):
+    print(f"Name:{name} Age:{age} City:{city}")
+
+student_info(age=  24, city="New", name="Mithun")
+
+
+#argbitory argument functions - it is used when you don't know arguments
+
+def add_all(*numbers):
+    total =0
+    for num in numbers:
+        total = total+ num
+    return total;
+
+print(add_all(1,2,4,5));
